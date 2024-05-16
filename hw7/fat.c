bd08a9f96731df19c691b3fec22512b5938b0038
Amina Ahmed <aminaahmed1803@gmail.com>
2024-05-16 19:29:39 -0400

    read finished

--- hw7/fat.c
+++ hw7/fat.c
@@ -244,23 +244,40 @@ size_t f_read(void *ptr, size_t size, size_t nmemb, file *stream){
     file_header *header = (file_header *)malloc(sizeof(file_header));
     memset(header, INITIALIZE, BLOCKSIZE);
     int offset = sb->data_offset + (stream->curr_FAT * BLOCKSIZE);
-    fseek(disk,offset,SEEK_SET);
-    fread(header,sizeof(file_header),1,disk);
-
-    if (header->size == 0){
-        free(header);
-        return 0;
-    }
+    
 
     size_t total_bytes = size * nmemb;
-    if (total_bytes > header->size){
-        total_bytes = header->size;
-    }
+    size_t bytes_read = 0;
+    uint16_t idx = stream->curr_FAT;
 
-    char *read_data = (char *)malloc(total_bytes);
-    memcpy(read_data, header->buffer + stream->read_bytes, total_bytes);
+    char *read_buffer = (char *)ptr;
 
-    ptr = (void *)read_data;
+    do {
+
+        fseek(disk,offset,SEEK_SET);
+        fread(header,sizeof(file_header),1,disk);
+
+        int already_read = stream->read_bytes;
+        int to_read = header->buffer_used - already_read;
+        memcpy(read_buffer, header->buffer + already_read, to_read);
+
+        bytes_read += to_read;
+        stream->read_bytes += to_read;
+
+        idx = header->next_FAT;
+        offset = sb->data_offset + (idx * BLOCKSIZE);
+
+        if (idx != -1){
+            stream->curr_FAT = idx;
+            stream->read_bytes = 0;
+            stream->write_bytes = 0;
+        }
+        
+    } while (idx != -1 && bytes_read < total_bytes);
+    
+    
+    free(header);
+    header = NULL;
     
     return total_bytes;
 }
@@ -437,28 +454,18 @@ int main(){
 
     file * h2 = f_open("/f2.txt", WRITE_ONLY);
 
-    for (int i=0 ;i<20 ; i++){
-        printf("%d ", fat_table[i]);
-
-    }printf("\n");
 
     char *buffer = "I like green apples. I like yellow apples. I like blue apples. I like purple apples. I like orange apples. I like pink apples. I like black apples. I like white apples. I like brown apples. I like grey apples. I like silver apples. I like gold apples. I like copper apples. I like bronze apples. I like brass apples. I like aluminum apples. I like iron apples. I like zebra apples. I like melon apples. I like watermelon apples. I like cantaloupe apples. I like honeydew apples. I like strawberry apples. I like raspberry apples. I like blueberry apples. I like blackberry apples. I like cranberry apples. I like cherry apples. I like peach apples. I like pear apples. I like plum apples. I like grape apples. I like kiwi apples. I like pineapple apples. I like mango apples. I like papaya apples. I like banana apples. I like coconut apples. I like orange apples. I like lemon apples. I like lime apples. I like grapefruit apples. I like tangerine apples. I like clementine apples. I like mandarin apples. I like kumquat apples. I like persimmon apples. I like pomegranate apples. I like avocado apples. I like olive apples. I like tomato apples. I like cucumber apples. I like zucchini apples. I like squash apples. I like pumpkin apples. I like eggplant apples. I like pepper apples. I like onion apples. I like garlic apples. I like ginger apples. I like turmeric apples. I like cinnamon apples. I like nutmeg apples. I like clove apples. I like allspice apples. I like cumin apples. I like coriander apples. I like cardamom apples. I like mustard apples. I like horseradish apples. I like wasabi apples. I like jalapeno apples. I like habanero apples. I like ghost apples. I like scorpion apples. I like reaper apples. I like cayenne apples. I like paprika apples. I like chili apples. I like pepper apples. I like salt apples. I like sugar apples. I like honey apples. I like syrup apples. I like molasses apples. I like caramel apples. I like chocolate apples. I like vanilla apples. I like coffee apples. I like tea apples. I like milk apples. I like cream apples. I like butter apples. I like cheese apples. I like yogurt apples. I like ice cream apples. I like sorbet apples. I like sherbet apples. I like gelato apples. I like custard apples. I like pudding apples. I like jello apples. I like mousse apples. I like souffle apples. I like cake apples. I like pie apples. I like tart apples. I like c";
 
-    //char *reader = (char *)malloc(strlen(buffer));
+    char *reader = (char *)malloc(strlen(buffer));
 
-    f_write(buffer, 1, strlen(buffer), h1);
+    //f_write(buffer, 1, strlen(buffer), h1);
 
-    f_write(buffer, 1, strlen(buffer), h2);
+    //f_write(buffer, 1, strlen(buffer), h2);
 
-    //f_read((void *) reader, 1, strlen(buffer), h1);
+    f_read((void *) reader, 1, 510, h1);
 
-    printf("file name: %s\n", h1->name);
-    printf("file size: %s\n", h2->name);
-
-    for (int i=0 ;i<20 ; i++){
-        printf("%d ", fat_table[i]);
-
-    }printf("\n");
+    printf("%s\n", reader);
 
     f_close(h1);
 
